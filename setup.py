57bb3eb0d758585473c5b73f53cfe8b06cbc265c
adas754 <anweshadas754@gmail.com>
2024-08-09 02:11:45 +0530

    First commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'munai',
+    author_email= 'anweshadas754@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
